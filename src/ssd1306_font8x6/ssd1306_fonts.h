b8a9901b93784d914c753520eb60c47a54dd16d2
jonssa <64277299+jonssa@users.noreply.github.com>
2020-06-27 13:22:24 +0200

    Add files via upload

--- src/ssd1306_font8x6/ssd1306_fonts.h
+++ src/ssd1306_font8x6/ssd1306_fonts.h
@@ -1,1 +1,26 @@
+/*
+ * ssd1306.h
+ *
+ *  Created on: 16 cze 2020
+ *      Author: Macias
+ */
 
+#include <stdint.h>
+
+#ifndef __SSD1306_FONTS_H__
+#define __SSD1306_FONTS_H__
+
+/*Struct containing font properties*/
+typedef struct {
+	const uint8_t FontWidth;
+	uint8_t FontHeight;
+	const uint8_t *data; //Pointer to data font data array
+} FontDef;
+
+
+extern FontDef Font6x8;
+//extern FontDef Font_7x10;
+//extern FontDef Font_11x18;
+//extern FontDef Font_16x26;
+
+#endif // __SSD1306_FONTS_H__
